bb08e0a02008680238ace04c8a85ee40aafec41a
jakebusse <jakerbusse@gmail.com>
2021-10-23 20:26:01 -0500

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+== NECESSARY INSTALLS ==
+- pip3 install qrcode
+- pip3 install image
+- pip3 install opencv-python
+
+== RESOURCES USED ==
+https://www.pythontutorial.net/python-basics/python-check-if-file-exists/
+https://www.analyticsvidhya.com/blog/2021/07/3-interesting-python-projects-with-code-for-beginners/
+https://www.w3schools.com/python/python_file_remove.asp
--- main.py
+++ main.py
@@ -1,42 +1,58 @@
 import qrcode
 import cv2
+import os
 from os.path import exists
 
 def runApp():
-    print('Would you like to create or decode a QR code? (Type C (for create) or D (for decode)).')
+    print('Would you like to create a QR code, decode a QR code, or delete a QR code? (Type C (for create), D (for decode), or BS (for delete)).')
     action = input()
 
     if action.upper() == 'C':
-        create()
+        content = input("What do you want the QR Code to represent? ")
+        create(content)
     elif action.upper() == 'D':
         decode()
+    elif action.upper() == 'BS':
+        delete()
     else:
         print('INVALID RESPONSE')
         runApp()
 
-def create():
-    content = input("What do you want the QR Code to represent? ")
+def create(content):
     name = input("What do you want the file name to be (omit the extension)? ")
     file_exists = exists(name + ".jpg")
-    print(file_exists)
     if file_exists == True:
         print("FILE ALREADY EXISTS")
-        create()
-    else:
-        fileName = name
-        newCode = qrcode.make(content)
-        newCode.save(fileName + ".jpg")
+        replace = input("Would you like to replace it? (Answer Y/N) ")
+        if replace.upper() == "Y":
+            pass
+        else:
+            create(content)
+    newCode = qrcode.make(content)
+    newCode.save(name + ".jpg")
 
 def decode():
     name = input("File name of QR Code to decode (omit the extension): ")
     file_exists = exists(name + ".jpg")
     if file_exists == True:
-        fileName = name
         d = cv2.QRCodeDetector()
-        val, _, _ = d.detectAndDecode(cv2.imread(fileName + ".jpg"))
+        val, _, _ = d.detectAndDecode(cv2.imread(name + ".jpg"))
         print("Decoded text is: ", val)
     else:
         print("FILE DOES NOT EXIST")
-        decode()
+        createFile = input("Would you like to create a new QR code with this file name. (Answer Y/N) ")
+        if createFile.upper() == "Y":
+            create()
+        else:
+            decode()
+
+def delete():
+    name = input("File name of QR Code to be deleted (omit extension): ")
+    file_exists = exists(name + ".jpg")
+    if file_exists == True:
+        os.remove(name + ".jpg")
+    else:
+        print("FILE DOES NOT EXIST")
+        runApp()
 
 runApp()
